920521f94e12461ca7b83a58ae37458e1755b73f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-12-01 06:17:33 +0100

    refactors local vars

--- 01/volumeCredits.ts
+++ 01/volumeCredits.ts
@@ -2,10 +2,10 @@ import { playFor } from './playFor.ts';
 import { PlayPerformance } from './types.ts';
 
 export function volumeCredits(perf: PlayPerformance) {
-  let volumeCredits = 0;
+  let result = 0;
   // add volume credits
-  volumeCredits += Math.max(perf.audience - 30, 0);
+  result += Math.max(perf.audience - 30, 0);
   // add extra credit for every ten comedy attendees
-  if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+  if ('comedy' === playFor(perf).type) result += Math.floor(perf.audience / 5);
+  return result;
 }
