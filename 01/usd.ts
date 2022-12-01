e578dd83840f5730f24851e5dd32ed1f15fc4d4d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-12-01 06:57:50 +0100

    refactors to usd

--- 01/usd.ts
+++ 01/usd.ts
@@ -1,7 +1,7 @@
-export function format() {
+export function usd(aNumber: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format;
+  }).format(aNumber);
 }
