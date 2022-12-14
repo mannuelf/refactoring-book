f781b3fba1cd491f8f6c178e0badc095c3cfa98c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-12-14 07:48:24 +0100

    makes PerformanceCalculator polymorphic: impliments amount calculation in new subclass, throws error in base class

--- 01/createStatementData.ts
+++ 01/createStatementData.ts
@@ -26,7 +26,15 @@ function createPerformanceCalculator(aPerformance: PlayPerformance, aPlay: Play)
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 class ComedyCalculator extends PerformanceCalculator {}
 
 export function createStatementData(invoice: Invoice, plays: Plays) {
